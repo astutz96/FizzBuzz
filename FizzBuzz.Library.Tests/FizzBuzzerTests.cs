dbe39d9e0687271e3186c88af9217f030f8786b0
astutz96 <astutz96.development@protonmail.com>
2019-10-27 21:53:46 -0400

    Condensing test cases using NUnit attributes

--- FizzBuzz.Library.Tests/FizzBuzzerTests.cs
+++ FizzBuzz.Library.Tests/FizzBuzzerTests.cs
@@ -4,26 +4,11 @@ namespace FizzBuzz.Library.Tests
 {
     public class FizzBuzzerTests
     {
-
         [Test]
-        public void Buzzer_When1_Returns1()
+        public void Buzzer_WhenDefault_ReturnsInput([Values(1,2,4)]int input)
         {
-            //Arrange
-            int input = 1;
-
-            //Act
             string output = FizzBuzzer.GetValue(input);
-
-            //Assert
-            Assert.AreEqual("1", output);
-        }
-
-        [Test]
-        public void Buzzer_When2_Returns2()
-        {
-            int input = 2;
-            string output = FizzBuzzer.GetValue(input);
-            Assert.AreEqual("2", output);
+            Assert.AreEqual(input.ToString(), output);
         }
 
         [Test]
